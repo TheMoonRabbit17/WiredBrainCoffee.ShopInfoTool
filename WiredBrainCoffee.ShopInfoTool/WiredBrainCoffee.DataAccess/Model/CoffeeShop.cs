858e7e516e42e8b91b51b5deaa18d3b3309fbdbe
Whitney Taylor <whitney.taylor@azblue.com>
2023-03-08 15:03:29 -0700

    Updated the code for CoffeeShop

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -5,6 +5,6 @@
         public string Location { get; set; }
 
         public int BeansInStockInKg { get; set; }
-        public object BeansInStockInKg { get; set; }
+
     }
 }
--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -50,7 +50,7 @@ namespace WiredBrainCoffee.ShopInfoTool
                         {
                             var coffeeShop = foundCoffeeShops.Single();
                             Console.WriteLine($"> Location: {coffeeShop.Location}");
-                            Console.WriteLine($"> Beans in stock: {coffeeShop.BeansInStockInKg} kg");
+                            Console.WriteLine(value: $"> Beans in stock: {coffeeShop.BeansInStockInKg} kg");
                         }
                         else
                         {
